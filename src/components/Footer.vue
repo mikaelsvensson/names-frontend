297fd49807744b6f2f1cd8c1956a7cb6e5b4a2da
Mikael Svensson <mikael.svensson@klarna.com>
2020-12-19 14:43:23 +0100

    Authentication - Footer with log-out option #14

---
+++ src/components/Footer.vue
@@ -0,0 +1,30 @@
+<template>
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <b-button
+        v-if="token.value"
+        size="is-small"
+        type="is-text"
+        @click="logOut()"
+      >
+        Logga ut
+      </b-button>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+  inject: ['token'],
+  methods: {
+    logOut() {
+      this.token.value = null
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
